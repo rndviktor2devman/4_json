b954a2e59bc0acc9739782d0632d028abb9d32a5
rndviktor <Vitek_belsutovec@mail.ru>
2016-09-24 00:35:08 +0300

    implemented

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,22 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as data_file:
+    	data = json.loads(data_file.read())
+    return data
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, sort_keys=True, indent=2))
 
 
 if __name__ == '__main__':
-    pass
+	input_str = sys.argv[1]
+	if input_str.lower().endswith('.json'):
+		data = load_data(sys.argv[1])
+		pretty_print_json(data)
+	else:
+		raise NameError('Wrong file type!')
+    
